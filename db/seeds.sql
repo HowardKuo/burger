d70b3044e15f88a5637decbc1098cd2b74665f6c
HowardKuo <howardkuo91@gmail.com>
2019-06-01 18:56:18 -0700

    second commit - cats activity reference

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheese Burger');
+INSERT INTO burgers (name, consumed) VALUES ('Bacon Cheese Burger', true);
+INSERT INTO burgers (name, consumed) VALUES ('Double Double', true);
+INSERT INTO burgers (name, consumed) VALUES ('Fish Filet', true);
+INSERT INTO burgers (name) VALUES ('Pastrami');
